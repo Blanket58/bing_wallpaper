b6bbaf3db4ab80fe54bafef94fd7fe69538864fb
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-01-31 11:53:30 +0800

    Update ChangeWallPaper.bat

--- ChangeWallPaper.bat
+++ ChangeWallPaper.bat
@@ -4,7 +4,7 @@ set N=0
 :str
 set /a n=%n%+1
 
-reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\AutoWallPaper\1.jpg" /f
+reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\bing_autowallpaper\bing_autowallpaper\1.jpg" /f
 
 reg add "hkcu\control panel\desktop" /v WallpaperStyle /t REG_DWORD /d 0 /f
 
