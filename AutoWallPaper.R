d299823002f0e310ffd51e1d4c2b07b112f0637b
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-01-31 11:47:32 +0800

    Update AutoWallPaper.R

--- AutoWallPaper.R
+++ AutoWallPaper.R
@@ -1,9 +1,9 @@
 rm(list=ls())
 setwd("D:/AutoWallPaper/Pictures")
 packages<-.packages(all.available=T)
-if(!("RCurl" %in% packages)){install.packages(RCurl)}else{break}
-if(!("XML" %in% packages)){install.packages(XML)}else{break}
-if(!("stringr" %in% packages)){install.packages(stringr)}else{break}
+if(!("RCurl" %in% packages)){install.packages(RCurl)}
+if(!("XML" %in% packages)){install.packages(XML)}
+if(!("stringr" %in% packages)){install.packages(stringr)}
 library(RCurl)
 library(XML)
 library(stringr)
