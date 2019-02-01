56305d118c40f084bf5d19fcda7f35ad517ea1a3
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-01 16:11:47 +0800

    Update runR.bat

--- configs/runR.bat
+++ configs/runR.bat
@@ -1,4 +1,4 @@
 SET SF="HKEY_LOCAL_MACHINE\SOFTWARE\R-core\R"
 FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "InstallPath"') DO SET "APath=%%~J"
 cd %APath%\bin
-Rscript C:\Users\Blanket\Desktop\Rscripts\AutoWallPaper\bin\AutoWallPaper.R
+Rscript D:\bing_autowallpaper\bing_autowallpaper\bin\AutoWallPaper.R
