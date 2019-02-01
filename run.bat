3f998fb6b1e634bf845b44ac0647b11b0db1c164
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-01 16:32:09 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,16 +1,16 @@
 SET SF="HKEY_LOCAL_MACHINE\SOFTWARE\R-core\R"
 FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "InstallPath"') DO SET "APath=%%~J"
 cd %APath%\bin
-Rscript D:\bing_autowallpaper\bing_autowallpaper\bin\AutoWallPaper.R
+Rscript D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\AutoWallPaper.R
 echo Now Run the fetchname
-cd D:\bing_autowallpaper\bing_autowallpaper\pictures
-DIR *.* /B >D:\bing_autowallpaper\bing_autowallpaper\configs\list.txt
+cd D:\bing_autowallpaper-master\bing_autowallpaper-master\pictures
+DIR *.* /B >D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\list.txt
 echo Now run the ChangeWallPaper
 set Num=20
 set N=0
 :str
 set /a n=%n%+1
-reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\bing_autowallpaper\bing_autowallpaper\cache\1.jpg" /f
+reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\bing_autowallpaper-master\bing_autowallpaper-master\cache\1.jpg" /f
 reg add "hkcu\control panel\desktop" /v WallpaperStyle /t REG_DWORD /d 0 /f
 RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
 if "%n%"=="%Num%" goto end
