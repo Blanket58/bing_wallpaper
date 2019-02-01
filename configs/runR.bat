d32ac0b21a6472aa48d2a4c406982447fc71e8d2
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-01 16:08:13 +0800

    Create runR.bat

---
+++ configs/runR.bat
@@ -0,0 +1,4 @@
+SET SF="HKEY_LOCAL_MACHINE\SOFTWARE\R-core\R"
+FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "InstallPath"') DO SET "APath=%%~J"
+cd %APath%\bin
+Rscript C:\Users\Blanket\Desktop\Rscripts\AutoWallPaper\bin\AutoWallPaper.R
