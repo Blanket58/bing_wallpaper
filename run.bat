79bab21a57378a8dd7c13a495dfe89c3284f8e81
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-17 01:31:39 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "Inst
 echo %APath% >> D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt
 FOR /F "delims=:,tokens=1" %%i in (D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt) do %%i:
 cd %APath%\bin
-Rscript D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\AutoWallPaper.R
+Rscript D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\scratch.R
 echo Now Run the fetchname
 D:
 cd D:\bing_autowallpaper-master\bing_autowallpaper-master\pictures
