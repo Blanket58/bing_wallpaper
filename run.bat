e88e449b36e419abb8455436c9e73ce353c0da38
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-17 01:02:26 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,13 +9,14 @@ D:
 cd D:\bing_autowallpaper-master\bing_autowallpaper-master\pictures
 DIR *.* /B >D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\list.txt
 echo Now run the ChangeWallPaper
-set num=20
-set n=0
-:str
-set /a n=%n%+1
-reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\bing_autowallpaper-master\bing_autowallpaper-master\cache\1.jpg" /f
-reg add "hkcu\control panel\desktop" /v WallpaperStyle /t REG_DWORD /d 0 /f
-RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
-if "%n%"=="%num%" goto end
-goto str
-exit
+python D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\setwallpaper.py
+::set num=20
+::set n=0
+:::str
+::set /a n=%n%+1
+::reg add "hkcu\control panel\desktop" /v Wallpaper /d "D:\bing_autowallpaper-master\bing_autowallpaper-master\cache\1.jpg" /f
+::reg add "hkcu\control panel\desktop" /v WallpaperStyle /t REG_DWORD /d 0 /f
+::RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
+::if "%n%"=="%num%" goto end
+::goto str
+::exit
