ae1ad45c6eeef3e0f00e5a9e5f8973d4ba7ea849
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-17 01:02:20 +0800

    Update AutoWallPaper.R

--- bin/AutoWallPaper.R
+++ bin/AutoWallPaper.R
@@ -7,22 +7,26 @@ if(!("stringr" %in% packages)){install.packages(stringr)}
 library(RCurl)
 library(XML)
 library(stringr)
+namelist<-readLines("../configs/list.txt") %>% str_extract_all("^[[:alpha:]]+") %>% unlist
 #Random Page
 url<-"https://bing.ioliu.cn/?p=1"
 parsed.doc<-getURL(url) %>% htmlParse
 page.xpath<-"/html/body/div[4]/span"
 n<-xpathSApply(doc = parsed.doc,path = page.xpath,fun = xmlValue) %>% str_extract(" \\d+") %>% as.numeric
-i<-sample(1:n,1)
-url<-paste0("https://bing.ioliu.cn/?p=",i)
-#Random Picture
-parsed.doc<-getURL(url) %>% htmlParse
-xpath.count<-"//div[@class='item']"
-photo.num<-xpathSApply(doc = parsed.doc,path = xpath.count) %>% length
-j<-sample(1:photo.num,1)
-xpath<-paste0("/html/body/div[3]/div[",j,"]/div/img/@src")
-original.link<-xpathSApply(doc = parsed.doc,path = xpath) %>% str_split("\"")
-part.link<-str_split(original.link,"_")[[1]][-3]
-tag.name<-str_extract(part.link[1],"[[:alpha:]]+$")
+while(TRUE){
+  i<-sample(1:n,1)
+  url<-paste0("https://bing.ioliu.cn/?p=",i)
+  #Random Picture
+  parsed.doc<-getURL(url) %>% htmlParse
+  xpath.count<-"//div[@class='item']"
+  photo.num<-xpathSApply(doc = parsed.doc,path = xpath.count) %>% length
+  j<-sample(1:photo.num,1)
+  xpath<-paste0("/html/body/div[3]/div[",j,"]/div/img/@src")
+  original.link<-xpathSApply(doc = parsed.doc,path = xpath) %>% str_split("\"")
+  part.link<-str_split(original.link,"_")[[1]][-3]
+  tag.name<-str_extract(part.link[1],"[[:alpha:]]+$")
+  if(!(tag.name %in% namelist)) break
+}
 size<-"1920x1200.jpg"
 final.link<-paste(paste(part.link,collapse = "_"),size,sep = "_")
 jpgfile<-getBinaryURL(final.link)
