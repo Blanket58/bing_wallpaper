0215f6d5f2e2d9328b1ec063f4f09d12541ca9c7
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-18 13:06:47 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,12 +1,9 @@
-SET SF="HKEY_LOCAL_MACHINE\SOFTWARE\R-core\R"
-FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "InstallPath"') DO SET "APath=%%~J"
-echo %APath% >> D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt
-FOR /F "delims=:,tokens=1" %%i in (D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt) do %%i:
-cd %APath%\bin
-Rscript D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\scratch.R
-echo Now Run the fetchname
 D:
 cd D:\bing_autowallpaper-master\bing_autowallpaper-master\pictures
 DIR *.* /B >D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\list.txt
-echo Now run the ChangeWallPaper
+
+pip list >> D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\modules.txt
+
+Rscript D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\scratch.R
+
 python D:\bing_autowallpaper-master\bing_autowallpaper-master\bin\setwallpaper.py
