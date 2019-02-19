4fb97e6bd2b667622a04968ebc020b48f4f7ed4c
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-02-19 13:44:16 +0800

    Update searching.bat

--- configs/searching.bat
+++ configs/searching.bat
@@ -1,5 +1,5 @@
 SET SF="HKEY_LOCAL_MACHINE\SOFTWARE\R-core\R"
 FOR /F "tokens=2,*" %%I IN ('REG QUERY %SF% /v InstallPath 2^>NUL^|FIND /I "InstallPath"') DO SET "APath=%%~J"
-echo %APath% >> D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt
-FOR /F "delims=:,tokens=1" %%i in (D:\bing_autowallpaper-master\bing_autowallpaper-master\configs\Rpath.txt) do %%i:
+echo %APath% >> D:\bing_wallpaper-master\bing_wallpaper-master\configs\Rpath.txt
+FOR /F "delims=:,tokens=1" %%i in (D:\bing_wallpaper-master\bing_wallpaper-master\configs\Rpath.txt) do %%i:
 pause
