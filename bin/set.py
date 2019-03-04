e9a3290c28798c67ca6bdf47a5d67e13d582ef0a
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-03-04 22:02:30 +0800

    Update set.py

--- bin/set.py
+++ bin/set.py
@@ -1,13 +1,18 @@
 import win32api
 import win32gui
 import win32con
+import os
+
+
 def setwallpaper(pic):
-  # open register
-  regkey = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
-  win32api.RegSetValueEx(regkey,"WallpaperStyle", 0, win32con.REG_SZ, "0")
-  win32api.RegSetValueEx(regkey, "TileWallpaper", 0, win32con.REG_SZ, "0")
-  # refresh screen
-  win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,pic, win32con.SPIF_SENDWININICHANGE)
-if __name__=='__main__':
-  pic='../cache/cache.jpg'
-  setwallpaper(pic)
+    # open register
+    regkey = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(regkey, "WallpaperStyle", 0, win32con.REG_SZ, "0")
+    win32api.RegSetValueEx(regkey, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    # refresh screen
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, pic, win32con.SPIF_SENDWININICHANGE)
+
+
+if __name__ == '__main__':
+    pic = os.getcwd() + '\\cache\\cache.jpg'
+    setwallpaper(pic)
