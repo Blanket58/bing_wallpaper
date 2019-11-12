769c30e5a8f8bac4e9858712852efe477dcd40d4
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2019-11-12 14:03:12 +0800

    Update check_packages.R

--- bin/check_packages.R
+++ bin/check_packages.R
@@ -1,17 +1,17 @@
 # check R packages
 packages <- .packages(all.available = TRUE)
 repo <- 'https://mirrors.tuna.tsinghua.edu.cn/CRAN/'
-if(!any('RCurl' == packages)) install.packages('RCurl',repos = repo)
-if(!any('XML' == packages)) install.packages('XML',repos = repo)
-if(!any('stringr' == packages)) install.packages('stringr',repos = repo)
-if(!any('jsonlite' == packages)) install.packages('jsonlite',repos = repo)
-if(!any('shiny' == packages)) install.packages('shiny',repos = repo)
-if(!any('shinyjs' == packages)) install.packages('shinyjs',repos = repo)
+if(!any('RCurl' == packages)) install.packages('RCurl', repos = repo)
+if(!any('XML' == packages)) install.packages('XML', repos = repo)
+if(!any('stringr' == packages)) install.packages('stringr', repos = repo)
+if(!any('jsonlite' == packages)) install.packages('jsonlite', repos = repo)
+if(!any('shiny' == packages)) install.packages('shiny', repos = repo)
+if(!any('shinyjs' == packages)) install.packages('shinyjs', repos = repo)
 
 # check py modules
 library(stringr)
 modules<-readLines('configs/modules.txt') %>% str_split(' +')
-modules<-sapply(modules[-c(1,2)],function(x) x[1])
+modules<-sapply(modules[-c(1,2)], function(x) x[1])
 if(!any('pywin32' == modules)) system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pywin32')
 
 message('Done!\n')
